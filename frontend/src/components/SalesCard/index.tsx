e02564561db9c1cccb65ad402e84a1b6b41970f3
Jose Antonio <toni.farias@outlook.com>
2022-09-11 17:51:45 -0300

    Date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -21,10 +21,14 @@ function SalesCard() {
 
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/sales`).then(response => {
+
+        const dMin = minDate.toISOString().slice(0, 10)
+        const dMax = maxDate.toISOString().slice(0, 10)
+
+        axios.get(`${BASE_URL}/sales?minDate=${dMin}&maxDate=${dMax}`).then(response => {
             setSales(response.data.content)
         })
-    }, []);
+    }, [minDate, maxDate]);
 
 
     return (
